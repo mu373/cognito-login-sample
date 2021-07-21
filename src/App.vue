2f9a85dd65981152bad0b29f3009ab6a5749ecf2
Minami Ueda <minami.ueda@gmail.com>
2021-07-21 16:44:28 +0900

    Add Cognito login

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+      <div>
+          Cognito User Pools sample with external IDP
+      </div>
+      <div>
+          <a id="signInButton" href="javascript:void(0)" @click="userSignIn()" v-if="!isSignedIn" title="Sign in">Sign In / Sign Up</a>
+      </div>
+      <div>
+          <a id="signOutButton" href="javascript:void(0)" @click="userSignOut()" v-if="isSignedIn"  title="Sign Out">Sign Out</a>
+      </div>
+      <div><pre id="sessionInfo">{{sessionData}}</pre></div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Amplify from 'aws-amplify';
+import { Auth } from 'aws-amplify';
+import amplifyConfig from './amplifyConfig.js';
+
+Amplify.configure(amplifyConfig);
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+
+  data() {
+    return {
+      isSignedIn: false,
+      sessionData: '',
+    };
+  },
+
+  mounted() {
+    this.getSessionData()
+  },
+
+  methods: {
+
+    userSignIn() {
+      Auth.federatedSignIn();
+    },
+
+    userSignOut() {
+      Auth.signOut();
+    },
+
+    getSessionData() {
+      Auth.currentSession()
+        .then(session => {
+            this.sessionData = session;
+            this.isSignedIn = true;
+            console.log(session);
+        }).catch(e => {
+            console.log(e);
+        })
+    }
+
   }
 }
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: sans-serif;
 }
 </style>
